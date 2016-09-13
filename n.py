07f4ee12e5795f80df3f8b576a82bea812f8bd71
unknown <nurlanova_az@12-07.iuca.loc>
2016-09-13 17:10:22 +0600

    first commit

---
+++ n.py
@@ -0,0 +1,12 @@
+n = 1
+while n <= 100:
+	if (n % 3 == 0) and (n % 5 == 0):
+		print "FizzBuzz"
+	if n % 3 == 0:
+		print "Fizz"
+	if n % 5 == 0:
+		print "Buzz"
+	else: 
+		print str(n)
+	n = n + 1
+
